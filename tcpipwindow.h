d0f93ce8148e1af08533882724698c431ae3bcbd
eveningwyn <evening.wen@outlook.com>
2017-03-02 23:11:54 +0800

    1. update tcpipwindow.ui widget name 2. update tcpipwindow.cpp and tcpipwindow.h code

--- tcpipwindow.h
+++ tcpipwindow.h
@@ -3,16 +3,6 @@
 
 #include <QMainWindow>
 #include "tcpipserver.h"
-#include <QTreeWidgetItem>
-#include <QGroupBox>
-#include <QLabel>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QCheckBox>
-#include <QComboBox>
-#include <QGridLayout>
-#include <QTextBrowser>
-#include <QTextEdit>
 
 namespace Ui {
 class TcpIpWindow;
@@ -35,45 +25,12 @@ private slots:
 
 private:
     Ui::TcpIpWindow *ui;
-    QTreeWidgetItem *clientMode;
-    QTreeWidgetItem *serverMode;
     TcpIpServer *server;//服务器对象
     TcpIpClient *client;//客户端对象
     int serverReadlength;//服务器读取数据长度
     int serverWritelength;//服务器写入数据长度
     int clientReadlength;//客户端读取数据长度
     int clientWritelength;//客户端写入数据长度
-
-    /*****界面布局对象*****/
-    void createWidget();
-    QGridLayout *mainLayout;
-//IP、端口号设置
-    QGroupBox *ip_port_GroupBox;
-    QGridLayout *ip_port_SetGridLayout;
-    QLabel *ipLabel;
-    QLineEdit *ipLineEdit;
-    QLabel *portLabel;
-    QLineEdit *portLineEdit;
-    QCheckBox *portSetCheckBoxBtn;
-    QLineEdit *portSetLineEdit;
-    QPushButton *clientConnectBtn;
-//发送设置
-    QGroupBox *receiveSetGroupBox;
-    QGridLayout *receiveSetGridLayout;
-
-//接收设置
-    QGroupBox *sendSetGroupBox;
-    QGridLayout *sendSetGridLayout;
-
-//发送区域
-    QGroupBox *SendGroupBox;
-    QGridLayout *SendgridLayout;
-    QTextEdit *sendTextEdit;
-    QPushButton *sendBtn;
-//接收区域
-    QGroupBox *ReceiveGroupBox;
-    QGridLayout *ReceivegridLayout;
-    QTextBrowser *receiveShow;
 };
 
 #endif // TCPIPWINDOW_H
